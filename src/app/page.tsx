13060101d8dd6e3b1ea80ef6507434ff0ce0bd2b
= <=>
2024-09-15 13:24:22 +0700

    Connet API typeScrip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
 import AppHeader from "@/components/header/app.header";
 import MainSlider from "@/components/main/main.slider";
-import { sendRequestJS } from "@/utils/old.api"
+import { sendRequest } from "@/utils/api";
+
 
 import { Container } from "@mui/material";
 
 export default async function HomePage() {
 
 
-  const res = await sendRequestJS({
+  const res = await sendRequest<IBackendRes<IRequest[]>>({
     url: "http://locolhost:8000/api/v1",
     method: "POST",
     body: { category: "CHILL", limit: 1 }
   });
- 
-  console.log(res);
+
+  // console.log("Check type", res.data[0].url); 
 
   return (
     <div>
